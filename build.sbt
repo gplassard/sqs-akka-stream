f6ad9dd409f63d5b473a97fffc9a122043d351b1
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2024-02-23 01:50:08 +0100

    feat(init): int sum

--- build.sbt
+++ build.sbt
@@ -2,8 +2,16 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.4.0"
 
+val AkkaVersion = "2.8.5"
+val AwsVersion = "2.24.7"
+
 lazy val root = (project in file("."))
   .settings(
     name := "sqs-akka-stream",
-    idePackagePrefix := Some("fr.gplassard.sqsakkastream")
+    idePackagePrefix := Some("fr.gplassard.sqsakkastream"),
+    libraryDependencies := Seq(
+      "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+      "software.amazon.awssdk" % "sqs" % AwsVersion,
+      "software.amazon.awssdk" % "sso" % AwsVersion,
+    )
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,21 @@
 package fr.gplassard.sqsakkastream
 
-class Main extends App {
-  
+import akka.actor.ActorSystem
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Keep, Sink, Source}
 
+import scala.concurrent.ExecutionContext
+
+@main def main(args: String*): Unit = {
+  implicit val system: ActorSystem = ActorSystem("QuickStart")
+  implicit val ec: ExecutionContext = system.dispatcher
+
+  val source = Source(1 to 10)
+  val sink = Sink.fold[Int, Int](0)(_ + _)
+  val runnable = source.toMat(sink)(Keep.right).run()
+
+  runnable.onComplete(res => {
+    println(s"Complete $res")
+    system.terminate()
+  })
 }
